763eb8cc006611d1a08501bc701aee8981a35c4c
Debparna Dutta <115703800+accenture-deb@users.noreply.github.com>
2023-12-08 00:48:48 +0100

    added

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>This is Next and is deployed.</h1>
+    </main>
+  )
+}
